29fd57b5c12c7414e8f26e7d93de07c9f2e51d23
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2022-12-30 11:21:15 +0200

    add new model

--- models/user.js
+++ models/user.js
@@ -6,22 +6,23 @@ const { handleMongooseError } = require("../helpers");
 const emailRegexp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
 
 const userSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        match: emailRegexp,
-        unique: true,
-        required: true       
-    },
-    password: {
-        type: String,
-        minlength: 6,
-        required: true,
-    },
-    token: String
+  password: {
+    type: String,
+    minlength: 6,
+    required: [true, 'Set password for user'],
+  },
+  email: {
+    type: String,
+    match: emailRegexp,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ["starter", "pro", "business"],
+    default: "starter"
+  },
+  token: String
 }, { versionKey: false })
 
 userSchema.post("save", handleMongooseError)
