76a17dd3e6aefd04faf14cb52672a1fabfb2f153
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-01-10 20:35:24 +0200

    add avatar

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -13,7 +13,7 @@ const updateAvatar = async (req, res) => {
 
     await fs.rename(tmpUpload, resultUpload);
     const avatarURL = path.join("avatars", filename);
-    
+
     await User.findByIdAndUpdate(_id, { avatarURL });
 
     res.json({
