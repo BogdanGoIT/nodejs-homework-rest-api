eb8206eba64717676fa7c11d5bb6e2c3a1458d9f
BogdanGoIT <92575228+BogdanGoIT@users.noreply.github.com>
2023-01-20 01:44:17 +0200

    add verifyEmail

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,9 +1,10 @@
 const bcrypt = require("bcrypt");
 const gravatar = require('gravatar');
+const { nanoid } = require("nanoid");
 
 const { User } = require("../../models/user");
 
-const { HttpError } = require("../../helpers");
+const { HttpError, sendEmail } = require("../../helpers");
 
 const register = async (req, res) => {
 
@@ -18,8 +19,17 @@ const register = async (req, res) => {
 
     const hashPassword = await bcrypt.hash(password, 10);
     const avatarURL = gravatar.url(email);
+    const verificationToken = nanoid();
     
-    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL });
+    const newUser = await User.create({ ...req.body, password: hashPassword, avatarURL, verificationToken });
+
+    const verifyEmail = {
+        to: email,
+        subject: "Verify you email",
+        html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}">Click verify email</a>`
+    }
+
+    await sendEmail(verifyEmail);
 
     res.status(201).json({
         subscription: newUser.subscription,
